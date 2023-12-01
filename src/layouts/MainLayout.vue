499ad57312224821d576ec9031594bf6388b2292
elliotbradly <elliotbradly@gmail.com>
2023-11-30 22:11:13 -0500

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,13 +2,40 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated class="bg-primary text-white">
+    <q-header elevated class="bg-primary text-white headerCustomStyle">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
+          <q-avatar style="position:relative; top:34px;">
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Title
+          <span class="angle-font" style="position:relative; top:40px; left:5px">
+            Gator
+          </span>
+
+          <span class="new-art-font" style="position:relative; top:38px;">
+            Earth
+          </span>
+
+          <br>
+
+          <span class='pixily-font' style="position:relative; top:-20px; left:44px">
+
+            deployed by T>A>S>U>S>S
+
+          </span>
+
+          <br>
+
+          <span class='pixily-font' style="position:relative; top:-20px; left:44px">
+
+            deployed by T>A>S>U>S>S
+
+          </span>
+
+
+
+
+
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
@@ -30,3 +57,12 @@
 
   </q-layout>
 </template>
+
+
+<style scoped>
+.headerCustomStyle {
+  height: 60px !important;
+  display: flex;
+  align-items: center !important;
+}
+</style>
--- src/pages/000.index.vue
+++ src/pages/000.index.vue
@@ -1,5 +1,28 @@
 
 <template>
+
+<div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+
+</div>
+
+
   <h4>Post {{ postId }}</h4>
   <h4>Post {{ isError }}</h4>
   <h4>Post {{ isFetching }}</h4>
