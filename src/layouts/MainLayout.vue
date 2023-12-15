b050c2a5dd703e120b6778cdb289a8b10ebc16ca
elliotbradly <elliotbradly@gmail.com>
2023-12-15 18:48:15 -0500

    connect wallet not opening

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -36,84 +36,62 @@
 
 
         <div class="q-pa-md q-pr-m angle-font">
-    <q-btn-dropdown
-      class="glossy"
-      color="orange"
-      label="Connect"
-       outline rounded
-       size="l"
-    >
+          <q-btn-dropdown class="glossy" color="orange" label="Connect" outline rounded size="l">
 
 
 
-      <div class="row no-wrap q-pa-md q-gutter-y-lg">
-        <div class="column">
-          <div class="text-h6 q-mb-md angle-font text-orange">Connect Wallet</div>
+            <div class="row no-wrap q-pa-md q-gutter-y-lg">
+              <div class="column">
+                <div class="text-h6 q-mb-md angle-font text-orange">Connect Wallet</div>
 
 
-          <q-separator />
+                <q-separator />
 
-          <q-btn round>
-      <q-avatar size="32px">
-        <img src="/img/nami.png">
-      </q-avatar>
-    </q-btn>
+                <q-btn round @click="connectWallet(0, $event)">
+                  <q-avatar size="32px">
+                    <img src="/img/nami.png">
+                  </q-avatar>
+                </q-btn>
 
-    <q-separator />
+                <q-separator />
 
-    <q-btn round>
-      <q-avatar size="32px">
-        <img src="/img/eternl.png">
-      </q-avatar>
-    </q-btn>
+                <q-btn round @click="connectWallet(1, $event)">
+                  <q-avatar size="32px">
+                    <img src="/img/eternl.png">
+                  </q-avatar>
+                </q-btn>
 
+              </div>
 
-
-
+            </div>
+          </q-btn-dropdown>
         </div>
 
 
-      </div>
-    </q-btn-dropdown>
-  </div>
 
+        <div class="q-pa-md q-pr-m angle-font">
+          <q-btn-dropdown class="glossy" color="orange" label="Lore" outline rounded size="l">
+            <div class="row no-wrap q-pa-md">
+              <div class="column">
+                <div class="text-h6 q-mb-md">Settings</div>
+                <q-toggle v-model="mobileData" label="Use Mobile Data" />
+                <q-toggle v-model="bluetooth" label="Bluetooth" />
+              </div>
 
+              <q-separator vertical inset class="q-mx-lg" />
 
-  <div class="q-pa-md q-pr-m angle-font">
-    <q-btn-dropdown
-      class="glossy"
-      color="orange"
-      label="Lore"
-      outline rounded
-      size="l"
-    >
-      <div class="row no-wrap q-pa-md">
-        <div class="column">
-          <div class="text-h6 q-mb-md">Settings</div>
-          <q-toggle v-model="mobileData" label="Use Mobile Data" />
-          <q-toggle v-model="bluetooth" label="Bluetooth" />
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+
+                <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+                <q-btn color="primary" label="Logout" push size="sm" v-close-popup />
+              </div>
+            </div>
+          </q-btn-dropdown>
         </div>
-
-        <q-separator vertical inset class="q-mx-lg" />
-
-        <div class="column items-center">
-          <q-avatar size="72px">
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-
-          <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
-
-          <q-btn
-            color="primary"
-            label="Logout"
-            push
-            size="sm"
-            v-close-popup
-          />
-        </div>
-      </div>
-    </q-btn-dropdown>
-  </div>
 
 
       </q-toolbar>
@@ -145,3 +123,84 @@
   align-items: center !important;
 }
 </style>
+
+
+
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    const connectWallet = (val, evt) => {
+
+      alert(val)
+
+    }
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
