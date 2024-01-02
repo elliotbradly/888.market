37064fa8f0e116161133384f7681e0167a0ecf54
Brad Henderson <elliotbradly@gmail.com>
2024-01-02 14:06:52 -0500

    update

--- src/pages/000.index.vue
+++ src/pages/000.index.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="full-height row wrap justify-start items-start content-start">
-
     <canvas id="indexCanvas"> </canvas>
-
     <span v-if="isPending">Loading...</span>
     <span v-else-if="isError">Error: {{ error.message }}</span>
-
     <ul v-else-if="data">
       <pre>{{ JSON.stringify(data, null, 2) }}</pre>
     </ul>
-
   </div>
-
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted, onUpdated, inject, getCurrentInstance } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useQuery } from '@tanstack/vue-query'
-
-
 import { mount, update, unmount } from "../screens/horizontal-window"
-//import {mount, update, unmount } from "../composables/tiny-screen"
 
 var postId = 6;
 
@@ -38,29 +30,14 @@ const { isPending, isError, data, error } = useQuery({
 
 
 onMounted(async (props) => {
-  let { isPending, isError, isFetching, data, error } = useQuery({
-    queryKey: ['post', postId],
-    queryFn: () => fetcher(postId),
-  })
-
   mount('on')
-
-  //alert(window.electron.store.get('foo'));
 })
 
 onUpdated(async () => {
-  // text content should be the same as current `count.value`
-
   update('on')
-
-
 })
 
 onUnmounted(async () => {
-
-  //unmount('on')
-
-
 })
 
 
--- src/screens/horizontal-window.ts
+++ src/screens/horizontal-window.ts
@@ -1,53 +1,54 @@
-import { ref, onMounted, onUnmounted, onUpdated, inject, getCurrentInstance } from 'vue'
+import { ref, onMounted, onUnmounted, onUpdated, inject, getCurrentInstance } from "vue";
 
-import * as ActVsg from '../110.shade/01.visage.unit/visage.action'
-import * as ActCan from '../110.shade/03.container.unit/container.action'
-import * as ActTxt from '../110.shade/05.text.unit/text.action'
-import * as ActGph from '../110.shade/04.graphic.unit/graphic.action'
-import * as ActSpr from '../110.shade/06.sprite.unit/sprite.action'
-import * as ActHex from '../110.shade/07.hexagon.unit/hexagon.action'
-import * as ActFcg from '../110.shade/08.focigon.unit/focigon.action'
+import * as ActVsg from "../110.shade/01.visage.unit/visage.action";
+import * as ActCan from "../110.shade/03.container.unit/container.action";
+import * as ActTxt from "../110.shade/05.text.unit/text.action";
+import * as ActGph from "../110.shade/04.graphic.unit/graphic.action";
+import * as ActSpr from "../110.shade/06.sprite.unit/sprite.action";
+import * as ActHex from "../110.shade/07.hexagon.unit/hexagon.action";
+import * as ActFcg from "../110.shade/08.focigon.unit/focigon.action";
 
-export type HelloWorld = string | number
+export type HelloWorld = string | number;
 
 export const mount = async (value: HelloWorld) => {
-
-  console.log('sampleFunc:: ', value)
+  console.log("sampleFunc:: ", value);
 
   const instance = getCurrentInstance();
-  const SHADE = inject('SHADE')
+  const SHADE = inject("SHADE");
 
-  var bit = await SHADE['hunt'](ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: { height: 720 } });
+  var bit = await SHADE["hunt"](ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: { height: 320 } });
   instance?.proxy?.$forceUpdate();
 
-  window.addEventListener('resize', function (event) {
+  window.addEventListener(
+    "resize",
+    function (event) {
+      this.setTimeout(() => {
+        location.reload();
+      }, 333);
+    },
+    true
+  );
 
-    this.setTimeout(() => {
-      location.reload();
-    }, 333)
-
-  }, true);
-
-  return value
-}
+  return value;
+};
 
 export const update = async (value: HelloWorld) => {
-  console.log('sampleFunc:: ', value)
+  console.log("sampleFunc:: ", value);
 
   const instance = getCurrentInstance();
-  const SHADE = inject('SHADE')
+  const SHADE = inject("SHADE");
 
-  var bit = await SHADE['hunt'](ActVsg.REMOVE_VISAGE, { idx: "vsg00" })
-  bit = await SHADE['hunt'](ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: { height: 720 } })
+  var bit = await SHADE["hunt"](ActVsg.REMOVE_VISAGE, { idx: "vsg00" });
+  bit = await SHADE["hunt"](ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: { height: 320 } });
 
-  bit = await SHADE['hunt'](ActVsg.READ_VISAGE, { idx: "vsg00" })
+  bit = await SHADE["hunt"](ActVsg.READ_VISAGE, { idx: "vsg00" });
 
-  bit = await SHADE['hunt'](ActCan.WRITE_CONTAINER, { idx: "can00", src: 'vsg00' })
-  var container = bit.canBit.dat.bit
+  bit = await SHADE["hunt"](ActCan.WRITE_CONTAINER, { idx: "can00", src: "vsg00" });
+  var container = bit.canBit.dat.bit;
 
-  bit = await SHADE['hunt'](ActCan.SURFACE_CONTAINER, { idx: 'fce-can-00', src: "vsg00" });
+  bit = await SHADE["hunt"](ActCan.SURFACE_CONTAINER, { idx: "fce-can-00", src: "vsg00" });
 
-  bit = await SHADE['hunt'](ActCan.ADD_CONTAINER, { idx: "fce-can-00", dat: { bit: container } })
+  bit = await SHADE["hunt"](ActCan.ADD_CONTAINER, { idx: "fce-can-00", dat: { bit: container } });
 
   //bit = await SHADE['hunt']( ActTxt.WRITE_TEXT, { idx:'txt00', dat: {  txt: "text 00" }  })
   //bit = await SHADE['hunt']( ActCan.ADD_CONTAINER, { idx: "can00",  dat:{bit:bit.txtBit.dat.bit }})
@@ -64,28 +65,27 @@ export const update = async (value: HelloWorld) => {
   //bit = await SHADE['hunt']( ActSpr.WRITE_SPRITE, { idx:'spr00', dat: { src:'./img/000.png',  x:40, y:80 }  })
   //bit = await SHADE['hunt']( ActCan.ADD_CONTAINER, { idx: "can00",  dat:{bit:bit.sprBit.dat.bit }})
 
-  bit = await SHADE['hunt'](ActGph.WRITE_GRAPHIC, { idx: 'gph00', dat: { h: 100, w: 40, x: 40, y: 40 } })
-  bit = await SHADE['hunt'](ActCan.ADD_CONTAINER, { idx: "can00", dat: { bit: bit.gphBit.dat.bit } })
+  bit = await SHADE["hunt"](ActGph.WRITE_GRAPHIC, { idx: "gph00", dat: { h: 100, w: 40, x: 40, y: 40 } });
+  bit = await SHADE["hunt"](ActCan.ADD_CONTAINER, { idx: "can00", dat: { bit: bit.gphBit.dat.bit } });
 
-  bit = await SHADE['hunt'](ActGph.WRITE_GRAPHIC, { idx: 'gph01', dat: { h: 100, w: 40, x: 40, y: 40 } })
-  bit = await SHADE['hunt'](ActCan.ADD_CONTAINER, { idx: "can00", dat: { bit: bit.gphBit.dat.bit } })
+  bit = await SHADE["hunt"](ActGph.WRITE_GRAPHIC, { idx: "gph01", dat: { h: 100, w: 40, x: 40, y: 40 } });
+  bit = await SHADE["hunt"](ActCan.ADD_CONTAINER, { idx: "can00", dat: { bit: bit.gphBit.dat.bit } });
 
-  return value
-}
+  return value;
+};
 
 export const unmount = async (value: HelloWorld) => {
-  console.log('sampleFunc:: ', value)
+  console.log("sampleFunc:: ", value);
 
   const instance = getCurrentInstance();
-  const SHADE = inject('SHADE')
+  const SHADE = inject("SHADE");
 
-  console.log("unmounted..")
-  var bit = await SHADE['hunt'](ActVsg.REMOVE_VISAGE, { idx: "vsg00" })
+  console.log("unmounted..");
+  var bit = await SHADE["hunt"](ActVsg.REMOVE_VISAGE, { idx: "vsg00" });
 
-  return value
-}
+  return value;
+};
 
 export type Shade<Type> = {
   hunt: Function;
-} & Type
-
+} & Type;
