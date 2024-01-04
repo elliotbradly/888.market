f9166316883ca9fac88ed9ba4c01c568c188ae6d
Brad Henderson <elliotbradly@gmail.com>
2024-01-04 07:05:12 -0500

    poll

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,15 +1,19 @@
 <template>
-	<div :data-active="active" @dragenter.prevent="setActive" @dragover.prevent="setActive" @dragleave.prevent="setInactive" @drop.prevent="onDrop">
-		<slot :dropZoneActive="active"></slot>
-	</div>
+	
+  <span v-if="isPending">Loading...</span>
+    <span v-else-if="isError">Error: {{ error.message }}</span>
+    <ul v-else-if="data">
+      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+    </ul>
+
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-
+import { useQuery } from '@tanstack/vue-query'
 
 const readNow = async () =>
-  await fetch('writeBlock/').then((response) =>
+  await fetch('readNow/').then((response) =>
     response.json(),
   )
 
@@ -19,44 +23,12 @@ const { isPending, isError, data, error } = useQuery({
   refetchInterval: 1111
 })
 
-const emit = defineEmits(['files-dropped'])
-
-let active = ref(false)
-let inActiveTimeout = null
-
-// setActive and setInactive use timeouts, so that when you drag an item over a child element,
-// the dragleave event that is fired won't cause a flicker. A few ms should be plenty of
-// time to wait for the next dragenter event to clear the timeout and set it back to active.
-function setActive() {
-	active.value = true
-	clearTimeout(inActiveTimeout)
-}
-function setInactive() {
-	inActiveTimeout = setTimeout(() => {
-		active.value = false
-	}, 50)
-}
-
-function onDrop(e) {
-	setInactive()
-	emit('files-dropped', [...e.dataTransfer.files])
-}
-
-function preventDefaults(e) {
-	e.preventDefault()
-}
-
-const events = ['dragenter', 'dragover', 'dragleave', 'drop']
 
 onMounted(() => {
-	events.forEach((eventName) => {
-		document.body.addEventListener(eventName, preventDefaults)
-	})
+	
 })
 
 onUnmounted(() => {
-	events.forEach((eventName) => {
-		document.body.removeEventListener(eventName, preventDefaults)
-	})
+	
 })
 </script>
--- src/pages/000.index.vue
+++ src/pages/000.index.vue
@@ -1,33 +1,20 @@
 <template>
   <div class="full-height row wrap justify-start items-start content-start">
+    
+    <Poll></Poll>
     <canvas id="indexCanvas"> </canvas>
-    <span v-if="isPending">Loading...</span>
-    <span v-else-if="isError">Error: {{ error.message }}</span>
-    <ul v-else-if="data">
-      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
-    </ul>
+    
   </div>
 </template>
 
 <script setup>
+
+import Poll from '../components/Poll.vue'
+
 import { ref, onMounted, onUnmounted, onUpdated, inject, getCurrentInstance } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useQuery } from '@tanstack/vue-query'
 import { mount, update, unmount } from "../screens/horizontal-window"
-
-var postId = 6;
-
-const readNow = async () =>
-  await fetch('readNow/').then((response) =>
-    response.json(),
-  )
-
-const { isPending, isError, data, error } = useQuery({
-  queryKey: ['dat'],
-  queryFn: readNow,
-  refetchInterval: 1111
-})
-
 
 onMounted(async (props) => {
   mount('on')
