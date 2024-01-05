7717adab1632d8fef8ad7654998b8bf86d33b3e7
elliotbradly <elliotbradly@gmail.com>
2024-01-05 06:13:15 -0500

    update

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,11 +1,9 @@
 <template>
-
   <span v-if="isPending">Loading...</span>
-    <span v-else-if="isError">Error: {{ error.message }}</span>
-    <ul v-else-if="data">
-      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
-    </ul>
-
+  <span v-else-if="isError">Error: {{ error.message }}</span>
+  <ul v-else-if="data">
+    <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+  </ul>
 </template>
 
 <script setup>
@@ -14,8 +12,17 @@ import { useQuery } from '@tanstack/vue-query'
 
 var w = window.screen.width;
 
+const width = () => {
+  return window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth
+    || 0;
+}
+
 const readNow = async () =>
-  await fetch('readNow?w=111').then((response) =>
+
+
+  await fetch('readNow?w=' + width()).then((response) =>
     response.json(),
   )
 
