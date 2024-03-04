243982d422ddfccb7324ad0a00058e0a6726353a
Brad Henderson <elliotbradly@gmail.com>
2024-03-03 19:06:52 -0500

    building ui

--- src/pages/009.player.vue
+++ src/pages/009.player.vue
@@ -7,17 +7,33 @@
         allowfullscreen>
       </iframe>
 
-      <q-btn-group push class="absolute-center" style="position:relative; top:11px; left:320px">
-        
-      <q-btn outline label="left" @click="visitPage('map')" />
-      <q-btn outline label="forward" @click="visitPage('map')" />
-      <q-btn outline  label="backward" @click="visitPage('map')" />
-      <q-btn outline label="right" @click="visitPage('map')" />
-        
+      <q-btn-group push class="absolute-center" style="position:relative; top:15px; left:320px">
+
+        <q-btn outline label="left" @click="visitPage('map')" />
+        <q-btn outline label="forward" @click="visitPage('map')" />
+        <q-btn outline label="backward" @click="visitPage('map')" />
+        <q-btn outline label="right" @click="visitPage('map')" />
 
       </q-btn-group>
 
-      
+  
+
+ <div class="q-pa-md example-break-row">
+
+    <div class="row items-start example-container">
+      <div class="example-cell" tabindex="0">Col 1 / Row 1</div>
+      <div class="example-cell col-6" tabindex="0">Col 2 / Row 1 - 1<br>Col 2 / Row 1 - 2</div>
+      <div class="flex-break"></div>
+      <div class="example-cell" tabindex="0">Col 1 / Row 2</div>
+      <div class="flex-break q-py-md"></div>
+      <div class="example-cell col-4" tabindex="0">Col 1 / Row 3</div>
+      <div class="example-cell" tabindex="0">Col 2 / Row 3</div>
+      <div class="example-cell" tabindex="0">Col 3 / Row 3</div>
+    </div>
+
+  </div>
+
+
 
     </div>
 
@@ -41,7 +57,7 @@ var lorem = 'Lorem ipsum dolor, sit';
 const router = useRouter()
 
 var action = (idx) => {
- //router.push('/' + idx)
+  //router.push('/' + idx)
 }
 
 
