1f84686c755ceb640a30de90035144a7e6b57fbf
Brad Henderson <elliotbradly@gmail.com>
2024-04-25 13:49:19 -0400

    update

--- 888.market/97.collect.unit/collect.model.ts
+++ 888.market/97.collect.unit/collect.model.ts
@@ -3,7 +3,7 @@ import Collect from "./fce/collect.interface";
 import CollectBit from "./fce/collect.interface";
 
 export class CollectModel implements Collect {
- idx:string;
+ idx:string = '23.11.14';
  caboodleBitList: CaboodleBit[] = [];
  caboodleBits: any = {};
 }
