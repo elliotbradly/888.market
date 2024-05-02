694d473713dcada879217096bf967bd3cc593a03
Brad Henderson <elliotbradly@gmail.com>
2024-05-02 12:15:53 -0400

    considering the creation of an example orb

--- src/pages/015.admin.vue
+++ src/pages/015.admin.vue
@@ -6,90 +6,97 @@
 
       <div class="col-3" style="overflow: auto; min-width: 200px; max-width: 200px;">
 
-        <q-btn outline color="text-primary" class="full-width" @click="action('sower')">Orb Sower</q-btn>
 
-        <q-chip
-        color="red"
-        text-color="white"
-        class="absolute-bottom-left"
-      >
-        <q-avatar>
-          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-        </q-avatar>
-        <div class="ellipsis">
-           cookiesLabel 
-          <q-tooltip>  cookiesLabel </q-tooltip>
-        </div>
-      </q-chip>
+        <q-card class="my-card" flat bordered>
+
+          <q-separator color="primary" spaced="true" inset="false" size="15px" />
+          <q-card-section>
+            <div class="text-subtitle2">
+              No Orb Present
+            </div>
+          </q-card-section>
+          <q-separator color="primary" spaced="true" inset="false" size="15px" />
+        </q-card>
+
+
+
+
+        <q-btn outline color="text-primary" class="full-width absolute-bottom-left" @click="action('title')">
+          View Orb
+        </q-btn>
+
+        <q-chip color="primary" text-color="white" class="absolute-bottom-left">
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+          <div class="ellipsis">
+            Member
+            <q-tooltip> Member </q-tooltip>
+          </div>
+        </q-chip>
 
       </div>
 
       <div class="col-grow" style="overflow: auto;">
 
-        <h6>Admin</h6>
 
-        <q-input outlined v-model="text" label="Title Name" />
+        <div class="fixed-center" style="min-width: 300px">
 
-        <q-separator color="black" spaced="true" inset="false" size="30px" />
-        
+          <q-separator color="primary" spaced="true" inset="false" size="30px" />
+          <q-input outlined v-model="text" label="Orb Name" />
+          <q-separator color="primary" spaced="true" inset="false" size="30px" />
+
+          <q-btn outline color="text-primary" class="full-width" @click="action('')">
+            Create Orb
+          </q-btn>
+
+          <q-separator color="primary" spaced="true" inset="false" size="10px" />
+          <q-btn outline color="text-primary" class="full-width" @click="action('')">
+            Create Example Orb
+          </q-btn>
+
+
+        </div>
+
+
       </div>
 
       <div style="overflow: auto; min-width: 200px; max-width: 360px;">
 
-        <q-btn outline color="text-primary" class="full-width" @click="action('title')">View Orb</q-btn>
-
         <div class="q-pa-md" style="max-width: 350px">
-    <q-list padding bordered class="rounded-borders">
-      <q-expansion-item
-        dense
-        dense-toggle
-        expand-separator
-        icon="perm_identity"
-        label="Account settings"
-      >
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+          <q-list padding bordered class="rounded-borders">
+            <q-expansion-item dense dense-toggle expand-separator icon="perm_identity" label="Characters">
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
 
-      <q-expansion-item
-        dense
-        dense-toggle
-        expand-separator
-        icon="signal_wifi_off"
-        label="Wifi settings"
-      >
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+            <q-expansion-item dense dense-toggle expand-separator icon="signal_wifi_off" label="Players">
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
 
-      <q-expansion-item
-        dense
-        dense-toggle
-        expand-separator
-        icon="drafts"
-        label="Drafts"
-        header-class="text-purple"
-      >
-        <q-card>
-          <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-    </q-list>
-  </div>
+            <q-expansion-item dense dense-toggle expand-separator icon="drafts" label="Dialogue"
+              header-class="text-primary">
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-list>
+        </div>
 
       </div>
 
@@ -108,7 +115,7 @@ const text = ref('')
 const router = useRouter()
 
 var action = (idx) => {
-  router.push('/' + idx )
+  router.push('/' + idx)
 }
 
 </script>
